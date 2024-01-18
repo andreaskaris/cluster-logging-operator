4caea979e4fd9c5b5d84569d0bebabfd503e881e
Andreas Karis <ak.karis@gmail.com>
2024-01-18 12:04:17 +0100

    Catch nil ptr exceptions with enabled Forwarder and empty Collection
    When the Forwarder is enabled, make sure that
    clusterLogging.Spec.Collection is non nil to avoid nil pointer
    exceptions from occurring.
    
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- internal/validations/clusterlogforwarder/validate_clusterlogging_dependency.go
+++ internal/validations/clusterlogforwarder/validate_clusterlogging_dependency.go
@@ -9,7 +9,7 @@ import (
 
 func ValidateClusterLoggingDependency(clf loggingv1.ClusterLogForwarder, k8sClient client.Client, extras map[string]bool) (error, *loggingv1.ClusterLogForwarderStatus) {
 	if clf.Name == constants.SingletonName && clf.Namespace == constants.OpenshiftNS && !extras[constants.ClusterLoggingAvailable] {
-		return errors.NewValidationError("ClusterLogForwarder '%s/%s' is dependent on a ClusterLogging instance", constants.OpenshiftNS, constants.SingletonName), nil
+		return errors.NewValidationError("ClusterLogForwarder '%s/%s' is dependent on a ClusterLogging instance with a valid Collector configuration", constants.OpenshiftNS, constants.SingletonName), nil
 	}
 	return nil, nil
 }
